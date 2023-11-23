62ce7f092f372fccc6ff29f355a8cb8157fb4c8c
Amr Elsehety <amr.abdelazeem22@gmail.com>
2023-11-23 18:51:04 +0300

    Select button with same id as rand-color

--- game.js
+++ game.js
@@ -1,10 +1,12 @@
 var gamePattern = [];
 var buttonColors = ["red", "blue", "green", "yellow"];
 
-function nextSequence(){
-    var randomNumber = Math.floor((Math.random() * 3)+1);
-    console.log(randomNumber);
-}
+function nextSequence() {
 
+}
+var randomNumber = Math.floor(Math.random() * 3 + 1);
 var randomChoosenColor = buttonColors[randomNumber];
 gamePattern.push(randomChoosenColor);
+console.log(randomChoosenColor);
+
+$("#"+randomChoosenColor).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
