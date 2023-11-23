87b20a375534805e8e7d3d8eccd61a3e6a21fa54
Amr Elsehety <amr.abdelazeem22@gmail.com>
2023-11-23 19:43:01 +0300

    Create 2 functions for sound and animation

--- game.js
+++ game.js
@@ -1,12 +1,40 @@
 var gamePattern = [];
+var userClickedPattern = [];
 var buttonColors = ["red", "blue", "green", "yellow"];
 
 function nextSequence() {
+  var randomNumber = Math.floor(Math.random() * 3 + 1);
+  var randomChoosenColor = buttonColors[randomNumber];
+  gamePattern.push(randomChoosenColor);
+
+  $("#" + randomChoosenColor)
+  .fadeOut(100)
+  .fadeIn(100)
+  .fadeOut(100)
+  .fadeIn(100);
+
+  playSound(randomChoosenColor);
+  animatePress(randomChoosenColor);
 
 }
-var randomNumber = Math.floor(Math.random() * 3 + 1);
-var randomChoosenColor = buttonColors[randomNumber];
-gamePattern.push(randomChoosenColor);
-console.log(randomChoosenColor);
+$(".btn").click(function(){
+    var userChoosenColor = $(this).attr("id");
+    console.log(userChoosenColor);
+    userClickedPattern.push(userChoosenColor);
 
-$("#"+randomChoosenColor).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+    playSound(userChoosenColor);
+    animatePress(userChoosenColor);
+ })
+
+function playSound(name){
+    var audio = new Audio("sounds/" + name + ".mp3");
+    audio.play();
+}
+
+function animatePress(currentColor){
+    $("."+currentColor).addClass("pressed");
+
+    setTimeout(function(){
+        $("."+currentColor).removeClass("pressed")
+    }, 100);
+}
